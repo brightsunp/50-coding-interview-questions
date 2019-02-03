3aab7c5a7f210a7b7455a4dfba2fcaa7c6e29011
sunp <sunp@whu.edu.cn>
2019-02-03 16:40:25 +0800

    update 46

---
+++ string/46_String-Deletion.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# coding=utf-8
+'''
+__author__ = 'sunp'
+__date__ = '2019/2/3'
+
+Given a string and a dictionary HashSet, write a function to determine the minimum number of characters to delete to make a word.
+
+dictionary: [“a”, “aa”, “aaa”]
+query: “abc”
+output: 2
+'''
+
+
+if __name__ == '__main__':
+    pass
